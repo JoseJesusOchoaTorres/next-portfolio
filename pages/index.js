829e26f051ea5b9972e00ffb773a9b25eeec4c6c
Jose Jesus Ochoa Torres <Jose.Jesus.Ochoa@ibm.com>
2019-08-01 00:58:06 -0500

    Updating my title message.

--- pages/index.js
+++ pages/index.js
@@ -6,83 +6,45 @@ import Nav from '../components/nav'
 const Home = () => (
   <div>
     <Head>
-      <title>Home</title>
+      <link rel="stylesheet" type="text/css" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css" />
+      <title>🐱Chetito</title>
     </Head>
 
-    <Nav />
-
     <div className='hero'>
-      <h1 className='title'>Welcome to Next.js!</h1>
-      <p className='description'>
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className='row'>
-        <Link href='https://github.com/zeit/next.js#getting-started'>
-          <a className='card'>
-            <h3>Getting Started &rarr;</h3>
-            <p>Learn more about Next.js on GitHub and in their examples</p>
-          </a>
-        </Link>
-        <Link href='https://github.com/zeit/next.js/tree/master/examples'>
-          <a className='card'>
-            <h3>Examples &rarr;</h3>
-            <p>Find other example boilerplates on the Next.js GitHub</p>
-          </a>
-        </Link>
-        <Link href='https://github.com/zeit/next.js'>
-          <a className='card'>
-            <h3>Create Next App &rarr;</h3>
-            <p>Was this tool helpful? Let us know how we can improve it!</p>
-          </a>
-        </Link>
-      </div>
+      
+      <h1 className='title'>Sorry</h1>
+      <p className='description'>My cat is working with this page 🐱</p>
+      
     </div>
 
     <style jsx>{`
       .hero {
         width: 100%;
         color: #333;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background: linear-gradient(to right, #FDFBFB, #EBEDEE 70%);
       }
       .title {
+        font-family: sans-serif;
         margin: 0;
         width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
         text-align: center;
+        text-transform: uppercase;
+        background: linear-gradient(to right, #30CFD0 0%, #330867 100%);
+        webkit-background-clip: text;
+        background-clip: text;
+        webkit-text-fill-color: transparent;
+        -webkit-background-clip: text;  
+        color: transparent;
+        font-size: 15vmin;
       }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
+      .description {
+        font-family: sans-serif;
+        font-size: 1rem;
       }
     `}</style>
   </div>
