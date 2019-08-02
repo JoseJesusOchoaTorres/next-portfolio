f203070ca9a39e0093f469ec8087e97fbd8fb2bd
Jose Jesus Ochoa Torres <Jose.Jesus.Ochoa@ibm.com>
2019-08-02 00:28:37 -0500

    Generator mixing and color classes.

--- components/header.js
+++ components/header.js
@@ -3,14 +3,7 @@ import React from 'react'
 const Header = () => (
   <header>
     <div>
-        <p className="h1 large">H1</p>
-        <p className="h1 medium">H1</p>
-        <p className="h1">H1</p>
-        <p className="h2">H2</p>
-        <p className="h3">H3</p>
-        <p className="h4">H4</p>
-        <p>PARRAFO</p>
-        <p className="small">SMALL</p>
+        In progress ...
     </div>
 
     <style jsx>{`
