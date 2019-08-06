4dbd83ab11c0f05398db28f1f2676cede9700286
Jose Jesus Ochoa Torres <Jose.Jesus.Ochoa@ibm.com>
2019-08-06 01:06:05 -0500

    Grid system basic.

--- components/header.js
+++ components/header.js
@@ -2,12 +2,20 @@ import React from 'react'
 
 const Header = () => (
   <header>
-    <div className="padding-top-3 padding">
-        In progress ...
+    <div className="container">
+        <div className="row">
+            <div className="col-xs-1 red">Col-xs-1</div>
+            <div className="col-xs-11  green">Col-xs-11</div>
+        </div>
     </div>
 
     <style jsx>{`
-      
+        .red {
+            background: red;
+        }
+        .green {
+            background: green;
+        }
     `}</style>
   </header>
 )
