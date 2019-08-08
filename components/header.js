ef8570d1aeca49d2e317063eb0c8182053b3c963
Jose Jesus Ochoa Torres <Jose.Jesus.Ochoa@ibm.com>
2019-08-08 00:13:02 -0500

    Distribution classes.

--- components/header.js
+++ components/header.js
@@ -4,18 +4,12 @@ const Header = () => (
   <header>
     <div className="container">
         <div className="row">
-            <div className="col-xs-1 red">Col-xs-1</div>
-            <div className="col-xs-10 col-md-offset-1 green">Col-xs-11</div>
+            <h6>In progress ...</h6>
         </div>
     </div>
 
     <style jsx>{`
-        .red {
-            background: red;
-        }
-        .green {
-            background: green;
-        }
+       
     `}</style>
   </header>
 )
