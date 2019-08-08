609a30ee4e37f11243d0a5f6bdd2cd673b3b5ddd
Jose Jesus Ochoa Torres <Jose.Jesus.Ochoa@ibm.com>
2019-08-08 01:55:03 -0500

    Autoprefix, babel, prefix variable and more.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,39 @@
-// const withCSS = require('@zeit/next-css')
-// module.exports = withCSS({
-//     /* config options here */
-// })
+const path = require("path");
+const glob = require("glob");
 
-const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-    /* config options here */
-})
+module.exports = {
+    webpack: (config, {
+        dev
+    }) => {
+        config.module.rules.push({
+            test: /\.(css|scss)/,
+            loader: "emit-file-loader",
+            options: {
+                name: "dist/[path][name].[ext]"
+            }
+        }, {
+            test: /\.css$/,
+            use: ["babel-loader", "raw-loader", "postcss-loader"]
+        }, {
+            test: /\.s(a|c)ss$/,
+            use: [
+                "babel-loader",
+                "raw-loader",
+                "postcss-loader",
+                {
+                    loader: "sass-loader",
+                    options: {
+                        includePaths: ["scss", "node_modules"]
+                            .map(d => path.join(__dirname, d))
+                            .map(g => glob.sync(g))
+                            .reduce((a, c) => a.concat(c), [])
+                    }
+                }
+            ]
+        });
+        return config;
+    }
+};
+
+const withSass = require("@zeit/next-sass");
+module.exports = withSass();
