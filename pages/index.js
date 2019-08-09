853ae1acf1b37879105d3a3f19d2df0faebeda98
Jose Jesus Ochoa Torres <Jose.Jesus.Ochoa@ibm.com>
2019-08-09 02:06:57 -0500

    Header, first version done.

--- pages/index.js
+++ pages/index.js
@@ -9,61 +9,41 @@ import Header from '../components/header'
 import "../assets/css/main.scss";
 
 const Home = () => (
-  <div>
-    <Head>
-      <link rel="icon" href="../static/favicon.ico" sizes="32x32" type="image/png" />
-      <title>Jose Jesus &#8226; Software engineer</title>
-    </Head>
+	<div>
+		<Head>
+      		<link rel="icon" href="../static/favicon.ico" sizes="32x32" type="image/png" />
+      		<title>Jose Jesus &#8226; Software engineer</title>
+    	</Head>
 
-    <Header></Header>
+		<Header></Header>
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-        height: 100vh;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        background: linear-gradient(to right, #FDFBFB, #EBEDEE 70%);
-      }
-      .title {
-        background: linear-gradient(270deg, #30cfd0, #fb6767, #8a80d7, #d7b680);
-        background-size: 800% 800%;
-        animation: AnimationName 15s ease infinite;
-        font-family: sans-serif;
-        margin: 0;
-        text-align: center;
-        text-transform: uppercase;
-        webkit-background-clip: text;
-        background-clip: text;
-        webkit-text-fill-color: transparent;
-        -webkit-background-clip: text;  
-        color: transparent;
-        font-size: 15vmin;
-      }
-      .description {
-        font-family: sans-serif;
-        font-size: 1rem;
-      }
-      @-webkit-keyframes AnimationName {
-        0%{background-position:0% 50%}
-        50%{background-position:100% 50%}
-        100%{background-position:0% 50%}
-      }
-      @-moz-keyframes AnimationName {
-        0%{background-position:0% 50%}
-        50%{background-position:100% 50%}
-        100%{background-position:0% 50%}
-      }
-      @keyframes AnimationName { 
-        0%{background-position:0% 50%}
-        50%{background-position:100% 50%}
-        100%{background-position:0% 50%}
-      }
-    `}</style>
-  </div>
+		<style jsx global>{`
+			:root {
+				--black: #000;
+				--white: #FFF;
+				--gray-light-1: #FAFAFA;
+				--gray-light-2: #EFEFEF;
+				--gray-1: #999999;
+				--gray-2: #888888;
+				--gray-3: #666666;
+				--gray-dark-1: #444444;
+				--gray-dark-2: #333333;
+				--gray-dark-3: #111111;
+				--blue-1: #408bfc;
+				--blue-2: #0062ff;
+				--blue-3: #054ada;
+			}
+
+			body {
+				background-color: var(--white);
+				font-family: sans-serif, arial;
+				color: var(--gray-dark-2);
+				font-weight: lighter;
+				font-size: 16px;
+				line-height: 1;
+			}
+		`}</style>
+  	</div>
 )
 
 export default Home
